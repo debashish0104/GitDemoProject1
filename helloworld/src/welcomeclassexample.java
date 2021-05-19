4a73ea861d71dbb1cd8ae9afec978e72dfd5e005
nikkita gupta <nikkita gupta@DESKTOP-M7VSLFM>
2021-05-19 19:52:59 +0530

    first commit

---
+++ helloworld/src/welcomeclassexample.java
@@ -0,0 +1,10 @@
+
+public class welcomeclassexample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Debashish");
+	
+		System.out.println("Hello everyoneeeeeeeeeeee");
+	}
+
+}
